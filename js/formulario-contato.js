a78797faab054964760a4385ab020e00b84887c1
Emanuel <emanuelmartins1081@gmail.com>
2024-06-08 14:24:57 -0300

    Ajustar formulário

--- js/formulario-contato.js
+++ js/formulario-contato.js
@@ -2,10 +2,10 @@ function cadastrarFormulario(){
     let nome = document.getElementById("nome").value
     let email = document.getElementById("email").value
     let mensagem = document.getElementById("mensagem").value
-    
-    console.log(nome, email, mensagem)
 
     if(nome == "" || email == "" || mensagem == ""){
         alert("Os campos não podem estar vazios para poder enviar o formulário!")
+    } else{
+        console.log(nome, email, mensagem)
     }
 }
