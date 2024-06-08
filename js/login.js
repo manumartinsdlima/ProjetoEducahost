57b6ef43693133d8ec240aef07cf63a279ae0326
Emanuel <emanuelmartins1081@gmail.com>
2024-06-08 17:30:30 -0300

    Ajustar login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,10 @@
 function logar(){
    let email = document.getElementById("email").value 
    let senha = document.getElementById("senha").value 
+
+   if(email == "" || senha == ""){
+    alert("É preciso preencher os campos para logar!")
+   } else {
+    alert("Login efetuado com sucesso!")
+   }
 }
