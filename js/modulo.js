c7b38447f741583fcd5bfa0b3232cf0bd5793c26
Emanuel <emanuelmartins1081@gmail.com>
2024-06-11 23:43:52 -0300

    Ajustar modulo e logo abreviada

--- js/modulo.js
+++ js/modulo.js
@@ -5,6 +5,15 @@ const containerAtividade = document.querySelector(".container-atividade")
 
 let contadorContainerIndex = 0
 
+window.addEventListener("resize", () => {
+    const larguraTela = window.innerWidth
+
+    if (larguraTela > 770){
+        contadorContainerIndex = 0
+        containerModuloResponsivo.style.margin = "0 0 0 -" + contadorContainerIndex * 100 + "%"
+    }
+})
+
 function passarEsquerda(){
     contadorContainerIndex = contadorContainerIndex - 1
     if(contadorContainerIndex == -1){
@@ -31,6 +40,18 @@ function fecharContainerAtividade(){
     containerAtividade.style.display = "none"
 }
 
-function irJogo(){
-    window.location.href = "jogo.html"
+function irAtividade1(){
+    window.location.href = "atividade1.html"
+}
+
+function irAtividade2(){
+    window.location.href = "atividade2.html"
+}
+
+function irAtividade3(){
+    window.location.href = "atividade3.html"
+}
+
+function irAtividade4(){
+    window.location.href = "atividade4.html"
 }
