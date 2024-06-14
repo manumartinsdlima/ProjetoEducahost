73347cc3083eaea0cb9573e82471561363d6f798
Emanuel <emanuelmartins1081@gmail.com>
2024-06-13 21:07:14 -0300

    Implementação do jogo da memória

---
+++ js/jogo-memoria.js
@@ -0,0 +1,51 @@
+const emojis = ["😊", "😊", "😂", "😂", "❤️", "❤️", "👌", "👌", "😁", "😁", "👍", "👍", "🙌", "🙌", "😎", "😎"]
+
+let emojiEscolhido = emojis.sort(() => (Math.random() > .5) ? 2 : -1)
+
+for(let i = 0; i < emojis.length ; i++){
+   let box = document.createElement("div")
+    box.className = "box"
+    box.innerHTML = emojiEscolhido[i]
+
+    box.onclick = function(){
+        this.classList.add("box-aberta")
+
+        setTimeout(function(){
+            if(document.querySelectorAll(".box-aberta").length > 1){
+                if(document.querySelectorAll(".box-aberta")[0].innerHTML == document.querySelectorAll(".box-aberta")[1].innerHTML){
+                    document.querySelectorAll(".box-aberta")[0].classList.add("box-match")
+                    document.querySelectorAll(".box-aberta")[1].classList.add("box-match")
+
+                    document.querySelectorAll(".box-aberta")[1].classList.remove("box-aberta")
+                    document.querySelectorAll(".box-aberta")[0].classList.remove("box-aberta")
+
+                    if(document.querySelectorAll(".box-match").length == emojis.length){
+                        alert("Você venceu!")
+                    }
+                } else{
+                    document.querySelectorAll(".box-aberta")[1].classList.remove("box-aberta")
+                    document.querySelectorAll(".box-aberta")[0].classList.remove("box-aberta")
+                }
+            }
+        }, 500)
+    }
+
+    document.querySelector(".container-box").appendChild(box)
+}
+
+// const caixa = document.querySelector(".box")
+
+// const caixaLargura = caixa.getBoundingClientRect().width
+
+// console.log(caixaLargura)
+// caixa.style.height = caixaLargura
+
+// window.addEventListener("resize", () => {
+//     const caixaLargura = caixa.getBoundingClientRect().width
+//     const caixaAltura = caixa.getBoundingClientRect().height
+
+//     console.log(caixaLargura, caixaAltura)
+//     caixa.style.height = "caixaLargura"
+//     console.log(caixaLargura, caixaAltura)
+// })
+
